1e11c8fd22c9af4b3d70f307eafec9e38e1d324a
Sanskruti0609 <Sanskruti06.j@gmail.com>
2026-07-16 14:18:27 +0530

    feat: extract company cards and names

--- content.js
+++ content.js
@@ -1,1 +1,32 @@
 console.log("⚡ ScrapeBolt Loaded");
+
+const companyCards = document.querySelectorAll(".companies-list.mb-3");
+
+const companies = [];
+
+companyCards.forEach(card => {
+
+    const companyAnchor = card.querySelector(
+        "h2.ms-2.mb-0 a.text-reset.text-decoration-none"
+    );
+
+    const companyName = companyAnchor?.textContent.trim();
+    const companyLink = companyAnchor?.href;
+
+    const badges = card.querySelectorAll(
+    ".badge.badge-border.d-flex"
+    );
+    const cin = badges[0]?.textContent.trim();
+
+    const address = badges[1]?.textContent.trim();
+
+    const company = {
+    company_name: companyName,
+    company_link: companyLink,
+    cin: cin,
+    address: address
+};
+
+console.log(company);
+ 
+});
